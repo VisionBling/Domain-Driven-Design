beb589958f8ccf195bbac3cc67f91bb7c483eb1d
Uloma Okenyi <cyndibilo@gmail.com>
2023-10-27 06:58:58 +0100

    subscription status

--- Subscriptions/SubscriptionStatus.cs
+++ Subscriptions/SubscriptionStatus.cs
@@ -17,5 +17,7 @@ namespace Domain.Subscriptions
         public string Description { get; set; }
         public string Type { get; set; }
         public string TypeDescription { get; set; }
+
+
     }
 }
