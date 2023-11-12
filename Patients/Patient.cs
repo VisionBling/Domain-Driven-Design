44442ac48dbf1bc9e0e29c4f32af498f5de4b9b9
Uloma Okenyi <cyndibilo@gmail.com>
2023-11-12 10:21:27 +0000

    modified payment module

--- Patients/Patient.cs
+++ Patients/Patient.cs
@@ -12,15 +12,15 @@ namespace Domain.Patients
         public string Name { get; private set; }
         public string Species { get; private set; } // e.g., Dog, Cat
         public DateTime DateOfBirth { get; private set; }
-       // public ICollection<MedicalRecords> MedicalRecords { get; private set; }
+        public ICollection<MedicalRecords> MedicalRecords { get; private set; }
 
         public Patient(Guid id, string name, string species, DateTime dateOfBirth)
         {
             Id = id;
            Name = name;
-           Species = species;
-           DateOfBirth = dateOfBirth;
-           MedicalRecords = new List<MedicalRecords>();
+           //Species = species;
+           //DateOfBirth = dateOfBirth;
+          // MedicalRecords = new List<MedicalRecords>();
         }
 
         public void AddMedicalRecord(MedicalRecords record)
