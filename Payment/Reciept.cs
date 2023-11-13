7d9a488d619c7f5e6d6c2345b187bcd823a43c44
Uloma Okenyi <cyndibilo@gmail.com>
2023-11-13 07:06:45 +0000

    implemented Payment Module

--- Payment/Reciept.cs
+++ Payment/Reciept.cs
@@ -15,5 +15,6 @@ namespace Domain.Payment
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Address { get; set; }
     }
 }
