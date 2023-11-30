6cbdfdb37d781964bda0466cc62c5f2c83bacd40
Uloma Okenyi <cyndibilo@gmail.com>
2023-11-30 06:54:22 +0000

    constructor and methods

--- Subscriptions/Subscription.cs
+++ Subscriptions/Subscription.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Domain.Patients;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Domain.Subscriptions
 {
@@ -14,5 +16,15 @@ namespace Domain.Subscriptions
         public string SubscriptionType { get; set; }
 
         public DateTime CreatedDate { get; set; }
+
+        // Constructor and methods
+        public Subscription(Guid id, string name, string species, DateTime dateOfBirth)
+        {
+            Id = id;
+            Name = name;
+            Species = species;
+            DateOfBirth = dateOfBirth;
+            MedicalRecords = new List<MedicalRecords>();
+        }
     }
 }
