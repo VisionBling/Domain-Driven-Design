242701eea059b0afc7acbc26c180efba4329aa6d
Uloma Okenyi <cyndibilo@gmail.com>
2023-12-18 07:11:00 +0000

    modified subscription

--- Payment/Reciept.cs
+++ Payment/Reciept.cs
@@ -8,11 +8,9 @@ namespace Domain.Payment
 {
     public class Reciept
     {
-
-
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Address { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Address { get; set; }
     }
 }
