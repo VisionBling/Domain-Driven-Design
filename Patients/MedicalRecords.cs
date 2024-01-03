2162da64cbe7aa1f494c3ea48f208c1dfeef3c81
Uloma Okenyi <cyndibilo@gmail.com>
2024-01-03 08:45:07 +0000

    modified payment module

--- Patients/MedicalRecords.cs
+++ Patients/MedicalRecords.cs
@@ -12,6 +12,6 @@ namespace Domain.Patients
         //public string Name { get; set; }
        // public string Description { get; set; }
         //public DateTime Date { get; set; }
-       // public DateTime Time { get; set; }
+        public DateTime Time { get; set; }
     }
 }
