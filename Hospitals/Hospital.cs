58173b65438297b66c54619668ae982cf2a8aab1
Uloma Okenyi <cyndibilo@gmail.com>
2024-01-04 08:45:56 +0000

    modified payment module

--- Hospitals/Hospital.cs
+++ Hospitals/Hospital.cs
@@ -9,7 +9,7 @@ namespace Domain.Hospitals
     public class Hospital
     {
 
-     public int Id { get; set; }
+    // public int Id { get; set; }
      public string Name { get; set; }
     public string Description { get; set; }
     public DateTime Datecreated { get; set; }
