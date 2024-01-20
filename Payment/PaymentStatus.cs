e74b7d3597f1eb172f9569da96918c2fb8043910
Uloma Okenyi <cyndibilo@gmail.com>
2024-01-20 09:08:50 +0000

    modified payment module

--- Payment/PaymentStatus.cs
+++ Payment/PaymentStatus.cs
@@ -26,7 +26,7 @@ namespace Domain.Payment
       public string Fax { get; set; }
      public string FaxNumber { get; set;}
      public string PhoneNumber { get; set;}
-     // public string FaxName { get; set;}
+     public string FaxName { get; set;}
        // public string Email { get; set;}
     }
 }
