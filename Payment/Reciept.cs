ab226ac188926471ac3fd3da937638558ec4495d
Uloma Okenyi <cyndibilo@gmail.com>
2024-01-23 09:10:56 +0000

    modified payment module

--- Payment/Reciept.cs
+++ Payment/Reciept.cs
@@ -8,7 +8,7 @@ namespace Domain.Payment
 {
     public class Reciept
     {
-       // public int Id { get; set; }
+        public int Id { get; set; }
      //   public string? Name { get; set; }
        // public string? Description { get; set; }
 
