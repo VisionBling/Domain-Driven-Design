78057772ba2b04ac0283dcead436c25308bd9f12
Uloma Okenyi <cyndibilo@gmail.com>
2024-03-03 06:49:23 +0000

    modified payment module

--- Subscriptions/SubscriptionStatus.cs
+++ Subscriptions/SubscriptionStatus.cs
@@ -10,8 +10,8 @@ namespace Domain.Subscriptions
     {
 
         public int Id { get; set; }
-       // public string Name { get; set; }
-       // public string Description { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
         //public string Type { get; set; }
        // public string TypeDescription { get; set; }
 
