712d9f3e2fc4e4df6c86e519b2f2f3f8ce36a9bf
Uloma Okenyi <cyndibilo@gmail.com>
2024-03-13 07:54:16 +0000

    modified payment module

--- Payment/PaymentStatus.cs
+++ Payment/PaymentStatus.cs
@@ -14,9 +14,9 @@ namespace Domain.Payment
         public int Id { get; set; }
         public string Name { get; set; }
 
-       // public string Description { get; set; }
+        public string Description { get; set; }
 
-        //public string Type { get; set; }
+        public string Type { get; set; }
        // public string Address { get; set; }
       // public string City { get; set; }
        // public string Region { get; set; }
