c38e8e7068c5f5f2b458b867ac2ec8f04410cf20
Uloma Okenyi <cyndibilo@gmail.com>
2024-04-16 18:58:33 +0100

    modified hospital model

--- Hospitals/Hospital.cs
+++ Hospitals/Hospital.cs
@@ -12,7 +12,6 @@ namespace Domain.Hospitals
      public int Id { get; set; }
      public string Name { get; set; }
     public string Description { get; set; }
-    public DateTime Datecreated { get; set; }
 
     }
 }
