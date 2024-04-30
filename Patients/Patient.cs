8fdcf9bfcf9a94aba3f2fedcb2009afa3a063a39
Uloma Okenyi <cyndibilo@gmail.com>
2024-04-30 18:55:28 +0100

    modified Patient model

--- Patients/Patient.cs
+++ Patients/Patient.cs
@@ -8,14 +8,12 @@ namespace Domain.Patients
 {
     public class Patient
     {
-        public Patient() { }
         public Guid Id { get; private set; }
         public string Name { get; private set; }
         public string Species { get; private set; } // e.g., Dog, Cat
         public DateTime DateOfBirth { get; private set; }
         public ICollection<MedicalRecords> MedicalRecords { get; private set; }
 
-        // Constructor and methods
         public Patient(Guid id, string name, string species, DateTime dateOfBirth)
         {
             Id = id;
