b904e22ce2a798a896f9a2e414c0df2de62cf4f5
Uloma Okenyi <cyndibilo@gmail.com>
2024-05-02 18:58:17 +0100

    modified Patient model

--- Subscriptions/SubscriptionStatus.cs
+++ Subscriptions/SubscriptionStatus.cs
@@ -10,7 +10,6 @@ namespace Domain.Subscriptions
     {
 
 
-        public string Description { get; set; }
         public string Type { get; set; }
         public string TypeDescription { get; set; }
 
