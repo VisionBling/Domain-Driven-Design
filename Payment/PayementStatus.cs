509b81746b19cd6580cdb536efcd6a51c8a4d7d8
Uloma Okenyi <cyndibilo@gmail.com>
2024-05-07 19:12:12 +0100

    modified Patient model

--- Payment/PayementStatus.cs
+++ Payment/PayementStatus.cs
@@ -20,5 +20,8 @@ namespace Domain.Payment
         public string Address { get; set; }
         public string City { get; set; }
         public string Region { get; set; }
+
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
     }
 }
