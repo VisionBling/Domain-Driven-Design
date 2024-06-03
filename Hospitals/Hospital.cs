96e395c162059d6ae5be5d47efa85acf3611cb98
Uloma Okenyi <cyndibilo@gmail.com>
2024-06-03 05:44:52 +0100

    subscription module

--- Hospitals/Hospital.cs
+++ Hospitals/Hospital.cs
@@ -11,7 +11,7 @@ namespace Domain.Hospitals
 
      //public int Id { get; set; }
      //public string Name { get; set; }
-    //public string Description { get; set; }
+    public string Description { get; set; }
     public DateTime Datecreated { get; set; }
 
     }
