947d9a37a274a758c3f4197c10b7ec9047d6b745
Uloma Okenyi <cyndibilo@gmail.com>
2024-06-08 05:50:31 +0100

    subscription module

--- Patients/MedicalRecords.cs
+++ Patients/MedicalRecords.cs
@@ -10,7 +10,7 @@ namespace Domain.Patients
     {
        // public int Id { get; set; }
        // public string Name { get; set; }
-        //public string Description { get; set; }
+        public string Description { get; set; }
         public DateTime Date { get; set; }
         public DateTime Time { get; set; }
     }
